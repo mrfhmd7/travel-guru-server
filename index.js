18a38aab170cb4a9d8f29654a94a6eb8f0cabfa9
mrfhmd7 <mrf.hmd7@gmail.com>
2024-10-06 11:45:59 +0600

    fixed data issue

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const info = require('./assets/data/data.json');
+
 app.get('/', (req, res) => {
      res.send("Travel Guru is rinning");
+});
+
+app.get('/info', (req, res) => {
+     res.send(info)
 });
 
 app.listen(port, () => {
